e165441e8ab9bddedc0cacfe9b0e91242b93f098
Cliff Zhang <cliffzhang08@gmail.com>
2022-10-11 01:48:44 -0400

    Initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,24 @@
+import type { Configuration } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const config: Configuration = {
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/, /\.tsx?$/],
+        use: ["babel-loader"],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: "./src/index.html"
+  })]
+};
+
+export default config;
